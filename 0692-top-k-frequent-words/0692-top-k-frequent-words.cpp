5af72a7c879257c490963ec78fd2909e0a4eae5e
tanyanassa07 <tanyanassa07@gmail.com>
2026-08-15 23:41:17 +0530

    Time: 4 ms (44.07%), Space: 18.2 MB (47.48%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,33 +1,32 @@
-bool compare(pair<string,int>a,pair<string,int>b)
+class Solution {
+public:
+bool static comparator(pair<string,int>a,pair<string,int>b)
 {
 if(a.second==b.second)
 {
 if(a.first<b.first)
-{return true;}
-else
-{return false;}
+{return true;
+}
 }
 if(a.second>b.second)
 {return true;}
-else
-{return false;}
+return false;
 }
-class Solution {
-public:
-    vector<string> topKFrequent(vector<string>& words, int k)  {
-vector<string>answer;
-unordered_map<string,int>mpp;
-int i;
+    vector<string> topKFrequent(vector<string>& words, int k) {
 int n=words.size();
+int i;
+unordered_map<string,int>mpp;
 for(i=0;i<n;i++)
 {
 mpp[words[i]]++;
-}
-vector<pair<string,int>>v(mpp.begin(),mpp.end());
-sort(v.begin(),v.end(),compare);
-int j;
-for(j=0;j<k;j++)
-{answer.push_back(v[j].first);}
-return answer;    
+}    
+vector<pair<string,int>>pq;
+for(auto i : mpp)
+pq.push_back({i.first,i.second});
+sort(pq.begin(),pq.end(),comparator);
+vector<string>answer;
+for(i=0;i<k;i++)
+{answer.push_back(pq[i].first);}
+return answer;
     }
 };
